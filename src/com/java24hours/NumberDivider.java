0a67a16d2761e6c2955309d42b02efc2c269c71a
Rahulk <rahul.k.mbox1@gmail.com>
2019-03-18 14:48:05 +0530

    looking at different exceptions together

---
+++ src/com/java24hours/NumberDivider.java
@@ -0,0 +1,25 @@
+/*
+To test multiple exceptions
+ */
+package com.java24hours;
+
+/**
+ *
+ * @author Rahulk-gh
+ */
+public class NumberDivider {
+    public static void main(String[] args){
+        if (args.length==2){
+            int result = 0;
+            try{
+                result = Integer.parseInt(args[0])/Integer.parseInt(args[1]);
+                System.out.println("first argument divided by the second ar"
+                        + "gument is "+ result);
+            } catch (NumberFormatException exc){
+                System.out.println("Both arguments should be numbers");
+            }catch(ArithmeticException exc) {
+                System.out.println("cannot divide in that manner");
+            }
+        }
+    }
+}
